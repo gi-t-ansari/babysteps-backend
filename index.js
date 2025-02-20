82dafff314a556f46634ed0ffd9f60575fc3e287
Tanzeel Ansari <121967095+gi-t-ansari@users.noreply.github.com>
2025-02-21 02:16:12 +0530

    changed port

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("DB Connection Error:", err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
