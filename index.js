3decb0ba98fef126af175fe225ce56a8bf13f76a
gi-t-ansari <tanzeelshamsh.in@gmail.com>
2025-02-21 19:04:20 +0530

    fixed: CORS error

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 import "./models/Doctor.js";
 import "./models/Appointment.js";
 import doctorRoutes from "./routes/doctorRoutes.js";
@@ -9,6 +10,7 @@ import appointmentRoutes from "./routes/appointmentRoutes.js";
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/doctors", doctorRoutes);
